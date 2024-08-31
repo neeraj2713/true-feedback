b2e2aabcfaf7043b94e54fefce29508cb4f1a20d
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2024-08-31 13:41:06 +0530

    final commit

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: 'neeraj.shekhawat2003@gmail.com',
+      from: 'neerajshekhawat.tech',
       to: email,
       subject: 'Mystery Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
